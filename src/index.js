466370cb670a99808874c6dfd8ef62d8578d7171
Luigi-Teodoro <luigi.tc14@gmail.com>
2025-01-20 21:17:37 -0500

    hacer cambios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { dir } from 'console';
+
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
@@ -8,7 +8,7 @@ const app = express();
 app.__dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname , 'views'));
 app.set('view engine', 'ejs');
-app.set('/' , (req , res )=> ('index'));
+app.get('/' , (req,res) => res.render ('index'));
 app.listen(3000)
 console.log('server is running on http://localhost:3000');
 
