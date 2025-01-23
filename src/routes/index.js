324b7ca1734e2661785b083d448bf52516e3a524
seblee666 <sebastianramos965@gmail.com>
2025-01-23 01:05:25 -0500

    login y registro terminado con responsividad

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,6 @@ router.get('/', (req, res) => res.render('index', {title:'Inicio'}));
 router.get('/about', (req, res) => res.render('catalago', {title:'Catalogo'}));
 router.get('/contact', (req, res) => res.render('contactanos', {title:'Contactanos'}));
 router.get('/login', (req, res) => res.render('login', {title:'login'}));
+router.get('/register', (req, res) => res.render('register', {title:'Registro'}));
 
 export default router;
