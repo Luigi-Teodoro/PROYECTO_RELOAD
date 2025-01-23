767cd2bbb107e4f8e767efbe8ca1d7d97fc1abd4
seblee666 <sebastianramos965@gmail.com>
2025-01-23 00:38:20 -0500

    Iniciar sesion terminado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 
 import express from 'express';
-import { dirname, join } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import router from './routes/index.js';
 
 const app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.set('views', join(__dirname , 'views'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname , 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(router);
 app.listen(3000)
 console.log('server is running on http://localhost:3000');
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const router = Router();
 router.get('/', (req, res) => res.render('index', {title:'Inicio'}));
 router.get('/about', (req, res) => res.render('catalago', {title:'Catalogo'}));
 router.get('/contact', (req, res) => res.render('contactanos', {title:'Contactanos'}));
+router.get('/login', (req, res) => res.render('login', {title:'login'}));
 
 export default router;
